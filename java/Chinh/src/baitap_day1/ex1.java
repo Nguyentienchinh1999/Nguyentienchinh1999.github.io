706f99fc0b286e394b05d56fd21514de3765df95
Nguyentienchinh1999 <58553614+Nguyentienchinh1999@users.noreply.github.com>
2020-07-04 12:33:58 +0700

    ex1java

---
+++ java/Chinh/src/baitap_day1/ex1.java
@@ -0,0 +1,43 @@
+package baitap_day1;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ex1 bai1 = new ex1();
+		System.out.println("bài 1: ");
+		int tinhTong = bai1.total();
+		System.out.println("bài 2: ");
+		double giaiThua = bai1.factorial();
+		
+	}
+	static Scanner S = new Scanner(System.in);
+	public int total() {
+			
+		int num;
+		int total = 0;
+		do {
+			System.out.println("Nhập số:");
+
+			 num  = S.nextInt();
+			 if(num > 0) {
+				 total += num;
+				 System.out.println("tổng là: " + total);	 	 
+			 }
+				 
+			 
+		}while(total < 100);
+		return total;
+	}
+	public long factorial() {
+		int n = 10;
+		long fac = 1;
+		for(int i = 1; i <= n; i++) {
+			fac *= i;
+		}
+		System.out.println("gia thừa của 10 là: "+ fac);
+		return fac;
+	}
+}
