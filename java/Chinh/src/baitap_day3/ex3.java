e3b0c80adac108667d03e73ff06399d455eb60a5
Nguyentienchinh1999 <58553614+Nguyentienchinh1999@users.noreply.github.com>
2020-07-08 10:00:50 +0700

    exe3 modified

--- java/Chinh/src/baitap_day3/ex3.java
+++ java/Chinh/src/baitap_day3/ex3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ex3 {
 	public static void main(String[] args) {
 		insert();
-		coincide();
+		remove();
 		count();
 	}
 	static Scanner S = new Scanner(System.in);
@@ -15,35 +15,32 @@ public class ex3 {
 		for(int i = n - 1 ; i > 2; i--) {
 			
 			arr[i] =arr[i - 1];
-			System.out.println(i - 1);
+			
 		}
 		n++;
 		arr[2] = k;
 		System.out.println("mảng sau khi đã thêm : " + Arrays.toString(arr));
 	}
-	public static void coincide(){
-		 int[] arr = {90,90,20,27,35,22,10,8,8};
-		 int k;
-		 int n = arr.length;
-		 for(int i = 1; i < n; i++) {
-			 for(int j = 0; j < i; j++ ) {
-				 if(arr[i] == arr[j])	{
-					 for(k = i; k < n; k++ ) {
-						 arr[k] = arr[k + 1];
-						 n--;
-						 i--;
-					 }
-				 }
-			 }
+	public static void remove(){
+		int[] arr = {90,90,20,27,35,22,10,8,8};
+		int arrLength = arr.length;
+		for(int i=0; i<arrLength;i++) {
+			for(int j=i+1;j<arrLength;j++) {
+				if(arr[i] == arr[j]) {
+					arr[j] = arr[arrLength - 1];
+					arrLength --;
+				}
+			}
 		}
-		 System.out.println("mảng sau khi đã xóa các phần tử trùng: " + Arrays.toString(arr));
-		 
+		int[] newArr = Arrays.copyOf(arr, arrLength);
+		System.out.println(Arrays.toString(newArr));
 	}
 	
 	public static void count() {
-		int arr[] = new int[9];
+		int arr[] = new int[10];
+		int sum = 0;
 		double tienDien;
-		for(int i = 0; i < 9; i++ ) {
+		for(int i = 0; i < 10; i++ ) {
 			System.out.println("nhập vào  hộ gia đình thứ: " + i + ": ");
 			arr[i]  = S.nextInt();
 		if(arr[i] < 0) {
@@ -67,7 +64,9 @@ public class ex3 {
 	    	tienDien =  (arr[i] * 2.710);
 	    	System.out.println("tiền điện là: " + tienDien);
 	    }
+		sum += arr[i];
 	    }
+		System.out.println("tổng số tiền các hộ là: " + sum);
 	    }
 }
 
